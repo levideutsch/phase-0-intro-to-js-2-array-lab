af5ba427776fa5d4cce9dd1e2f8ee98d61c294d0
levideutsch <levideutsch@live.com>
2022-09-11 16:14:41 -0400

    commit tree dcf2b1ac08dc0eb15b41dac5c29cb62720c37d0e

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    console.log(name)
+    console.log(cats)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    console.log(name)
+    console.log(cats)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    console.log()
+    console.log(cats)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+
+}
+
+function appendCat(name) {
+    const catsCopy = [ ...cats]
+      catsCopy.push(name)
+      return catsCopy
+  }
+
+  function prependCat(name) {
+    const catsCopyTwo = [... cats]
+    catsCopyTwo.unshift(name)
+    return catsCopyTwo
+}
+
+function removeLastCat() {
+    const removeLastCatCopy = [...cats]
+    removeLastCatCopy.pop()
+    return removeLastCatCopy
+}
+
+function removeFirstCat() {
+    const removeFirstCatCopy = [...cats]
+    removeFirstCatCopy.shift()
+    return removeFirstCatCopy
+}
